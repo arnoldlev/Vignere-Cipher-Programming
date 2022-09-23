5353a33272c3817809ffc939b075c3a13aaa5acc
Arnold Lev <aalev@cpp.edu>
2022-09-22 20:41:05 -0700

    Done

--- Cipher.java
+++ Cipher.java
@@ -1,6 +1,58 @@
+import java.util.Scanner;
+
 public class Cipher {
 
+    public static int getNumber(char letter) {
+        return letter - 'A';
+    }
+
+    public static int getLetter(int num) {
+        return 'A' + num;
+    }
+
+    public static String encrpyt(String plain, String key) {
+        String cipher = "";
+        int keyLength = key.length();
+        int j = 0;
+
+        for (int i = 0; i < plain.length(); i++) {
+            int let = (getNumber(plain.charAt(i)) + getNumber(key.charAt(j))) % 26;
+            cipher += (char) getLetter(let);
+            if (j < keyLength - 1) {
+                j++;
+            } else {
+                j = 0;
+            }
+        }
+        return cipher;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Test");
+        Scanner scan = new Scanner(System.in);
+        String text, key;
+
+        if (args.length != 0) {
+           key = args[0];
+           text = args[1];
+           for (int i = 2; i < args.length; i++) {
+                text += " " + args[i];
+           }
+        } else {
+            System.out.println("Enter Plain Text: ");
+            text = scan.nextLine();
+
+            System.out.println("Enter Key:");
+            key = scan.nextLine();
+        }
+
+        String[] words = text.split(" ");
+        String sentence = "";
+        for (String e : words) {
+            sentence += encrpyt(e, key) + " ";
+        }
+
+        System.out.println("Encrypted: " + sentence);
+
+        scan.close();
     }
 }
